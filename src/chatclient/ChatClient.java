e15b87ea0b5f339fd4ab4e415a85f87d3f198cb5
JudeShun <bbjudespra44@gmail.com>
2019-12-01 21:20:54 +0800

    chat

---
+++ src/chatclient/ChatClient.java
@@ -0,0 +1,52 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package chatclient;
+
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.PrintWriter;
+import java.net.Socket;
+
+/**
+ *
+ * @author 2ndyrGroupB
+ */
+public class ChatClient 
+
+    /**
+     * @param args the command line arguments
+     */
+  {
+  public static void main(String[] args) throws Exception 
+  {
+     Socket sock = new Socket("172.16.32.26", 3000);
+                               // reading from keyboard (keyRead object)
+     BufferedReader keyRead = new BufferedReader(new InputStreamReader(System.in));
+                              // sending to client (pwrite object)
+     OutputStream ostream = sock.getOutputStream(); 
+     PrintWriter pwrite = new PrintWriter(ostream, true);
+ 
+                              // receiving from server ( receiveRead  object)
+     InputStream istream = sock.getInputStream();
+     BufferedReader receiveRead = new BufferedReader(new InputStreamReader(istream));
+ 
+     System.out.println("Start chatting, type and press Enter key");
+ 
+     String receiveMessage, sendMessage;               
+     while(true)
+     {
+        sendMessage = keyRead.readLine();  // keyboard reading
+        pwrite.println(sendMessage);       // sending to server
+        pwrite.  flush();                    // flush the data
+        if((receiveMessage = receiveRead.readLine()) != null) //receive from server
+        {
+            System.out.println(receiveMessage); // displaying at DOS prompt
+        }         
+      }               
+    }                    
+}                        
